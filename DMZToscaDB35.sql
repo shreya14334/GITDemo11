3b712b4f9814b5d5b3cab32d961d83acbc0722a6
shreya14334 <136710614+shreya14334@users.noreply.github.com>
2024-06-11 18:12:36 +0530

    Add files via upload

---
+++ DMZToscaDB35.sql
@@ -0,0 +1,5 @@
+
+
+select count(distinct(activityId)) from cwp_auxiliary where externalId = 'ON0005742820230417191250';
+
+select * from cwp_auxiliary where externalId = 'ON0005742820230417191250';
